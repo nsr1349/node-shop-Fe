dbc783f33c507f7c36eac09420f9a17fc41f4892
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-28 07:27:30 +0900

    feat: 모달 컴포넌트 정의

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom";
+
+const Modal = ({ children , isOpen , closeModal, className, ...props }) => {
+    return ReactDOM.createPortal(<>
+        {
+            isOpen &&         
+            <div className="fixed w-full h-full grid place-items-center z-40">
+                <div className="w-full h-full bg-[rgba(0,0,0,0.5)]" onClick={closeModal}/>
+                <div className={`fixed bg-main p-8 scale-up transition-all ${className}`} {...props} >
+                    {children}
+                </div>
+            </div>
+        }
+    </>, document.getElementById("modal") )
+};
+
+export default Modal;
