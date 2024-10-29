f3393698ac08605861bf7dd3a9d096fd4d130e0a
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-30 01:24:10 +0900

    feat: 페이지 일단 알아볼수 있을 정도로는 수정하기

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import Modal from "../../components/Modal/Modal";
 import { useModal } from "../../components/Modal/useModal";
 import AddProductForm from "./components/AddProductForm";
+import { RiAdminFill } from "react-icons/ri";
+import { AiFillProduct } from "react-icons/ai";
+import { MdReorder } from "react-icons/md";
 
 const AdminPage = () => {
     const { isOpen, openModal, closeModal } = useModal()
@@ -10,26 +13,26 @@ const AdminPage = () => {
         <Modal closeModal={closeModal} isOpen={isOpen}>
             <AddProductForm/>
         </Modal>
-        <aside className="h-screen sticky top-0 left-0 border-2 border-red-300 w-fit py-10 px-6">
-            <div>로고</div>
+        <aside className="h-screen sticky top-0 left-0 border-r-2 border-sub w-fit py-10 px-4">
+            <img src="/logo.png" alt="" className="w-24 mx-auto mb-6"/>
             <nav>
                 <ul>
                     <li>
-                        <Link className="flex gap-2">
-                            <div>아이콘</div>
+                        <Link className="flex gap-4 items-center  px-4 py-3 rounded-md mb-2 hover:bg-sub transition-all" >
+                            <RiAdminFill/>
                             <p>Admin account</p>
                         </Link>
                     </li>
                     <li>
-                        <Link className="flex gap-2">
-                            <div>아이콘</div>
-                            <p>Admin account</p>
+                        <Link className="flex gap-4 items-center px-4 py-3 bg-sub rounded-md mb-2 hover:bg-sub transition-all">
+                            <AiFillProduct/>
+                            <p>product</p>
                         </Link>
                     </li>
                     <li>
-                        <Link className="flex gap-2"> 
-                            <div>아이콘</div>
-                            <p>Admin account</p>
+                        <Link className="flex gap-4 items-center px-4 py-3 rounded-md mb-2 hover:bg-sub transition-all"> 
+                            <MdReorder/>
+                            <p>order</p>
                         </Link>
                     </li>
                 </ul>
@@ -37,12 +40,38 @@ const AdminPage = () => {
         </aside>
         <div className="min-h-screen flex-grow pt-20">
             <div className="border-2 border-red-900 w-11/12 max-w-5xl mx-auto">
-                <div>search bar</div>
+                <div className="mb-4">search bar</div>
                 <ul>
-                    <li>아이템</li>
+                    <li className="b-test flex">
+                        <div>
+                            <img src="" alt="" />
+                            <div>sku008</div>
+                        </div>
+                        <div className="flex-grow">
+                            <div className="flex gap-2">
+                                <h4>멋있는 자캣</h4>
+                                <div>ACTIVE</div>
+                            </div>
+                            <div className="flex gap-2">
+                                <div>icon</div>
+                                <div>32000 원</div>
+                            </div>
+                            <div className="flex gap-2">
+                                <div>icon</div>
+                                <ul className="flex gap-2">
+                                    <li>S : <span>56</span></li>
+                                    <li>S : <span>56</span></li>
+                                    <li>S : <span>56</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="flex items-end">
+                            <button className="b-test ">UPDATE</button>
+                            <button>DELETE</button>
+                        </div>
+                    </li>
                 </ul>
-                <button>추가버튼</button>
-                <button onClick={()=> openModal()}>열려라</button>
+                <button className="bg-sub mt-4 py-2 px-4 rounded-md hover:bg-g transition-all" onClick={()=> openModal()}>상품추가버튼</button>
             </div>
         </div>
     </main>
