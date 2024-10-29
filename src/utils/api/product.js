f1c02265712434db231d30e42596f74eb830a286
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-30 02:27:45 +0900

    feat: getProduct 요청 정의

--- src/utils/api/product.js
+++ src/utils/api/product.js
@@ -1,16 +1,24 @@
 import api from "./api";
 
 export const createProductApi = async ({ formData, image, categories, stocks}) => {
-        if (formData.price < 0) throw new Error('가격은 음수가 될 수 없습니다.')
-        if (!image) throw new Error('이미지는 필수 값입니다.')
+    if (formData.price < 0) throw new Error('가격은 음수가 될 수 없습니다.')
+    if (!image) throw new Error('이미지는 필수 값입니다.')
         
-        const stock = {}
-        stocks.forEach(({size ,qty})=> {
-            if (!size) throw new Error('상품 재고 사이즈칸을 확인 해주세요.')
-            if (qty < 0) throw new Error('수량은 음수가 될 수 없습니다.')
-            stock[size] = qty
-        })
-        const size = Object.keys(stock)
+    const stock = {}
+    stocks.forEach(({size ,qty})=> {
+        if (!size) throw new Error('상품 재고 사이즈칸을 확인 해주세요.')
+        if (qty < 0) throw new Error('수량은 음수가 될 수 없습니다.')
+        stock[size] = qty
+    })
+    const size = Object.keys(stock)
 
-        return await api.post("/product", { size , stock, image, categories, ...formData})
+    return await api.post("/product", { size , stock, image, categories, ...formData})
+}
+
+export const getProductApi = async () => {
+    try {
+        return await api.get("/product", )
+    } catch (error) {
+        return error
+    }
 }
