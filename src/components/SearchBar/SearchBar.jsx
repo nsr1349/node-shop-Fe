c25ec00d7f7cfe64666736303a5b8edc85aa8bac
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-11-01 00:07:55 +0900

    feat: 검색바 정의

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,18 @@
+import { MdSearch } from "react-icons/md";
+import { useNavigate } from "react-router-dom"; 
+
+const SearchBar = () => {
+    const Navigate = useNavigate()
+    const handleSearch = () => {
+        const q = document.querySelector('#search').value
+        Navigate('', { state : { q }})
+    }
+    return <div className="relative">
+        <input id='search' type="text" placeholder="검색어를 입력하세요" className="w-full" onKeyDown={({key})=> key === 'Enter' && handleSearch() }/>
+        <button className="absolute right-2 inset-y-0 text-3xl text-g hover:text-white transition-all" onClick={handleSearch}>
+            <MdSearch />
+        </button>
+    </div>
+}
+
+export default SearchBar;
