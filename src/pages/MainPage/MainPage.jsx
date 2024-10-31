b18380ee19364457fab262445996758b7e4b39a4
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-11-01 01:41:10 +0900

    feat: 패딩 좀 넣기

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -5,7 +5,7 @@ import MainProductList from "./MainProductList";
 const MainPage = () => {
     return <>
         <Layout>
-            <main className="max-w-[1000px] mx-auto">
+            <main className="max-w-[1000px] mx-auto px-4 pb-20">
                 <SearchBar/>
                 <MainProductList/>
             </main>
