0ad57b8a5d65cad8425f8d9c55785061b7928f2c
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-31 20:36:19 +0900

    feat: 펜딩버튼 디스에이블드 만듬

--- src/components/PendingButton/PendingButton.jsx
+++ src/components/PendingButton/PendingButton.jsx
@@ -1,5 +1,5 @@
 const PendingButton = ({children, isPending , className, ...props}) => {
-    return <button type="submit" className={className} {...props}>
+    return <button type="submit" disabled={isPending} className={className} {...props}>
         { isPending ? <div className="loader scale-50 mx-auto my-1"/> : children}
     </button>    
 }
