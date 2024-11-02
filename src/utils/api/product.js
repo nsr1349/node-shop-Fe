72c0d21aa88a843928acd3084c9920db1e6a2431
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-11-02 23:44:07 +0900

    feat: stock 을 바꾸는 방식으로 변경

--- src/utils/api/product.js
+++ src/utils/api/product.js
@@ -11,12 +11,12 @@ const productValidation = ({ sku , name, image, category, price, stocks }) => {
     if (category.length === 0) throw new Error('카테고리를 하나 이상 선택해주세요')
     
     const stock = {}
-    stocks.forEach(({size ,qty})=> {
+    stocks.sort((a, b) => sizeOptions.indexOf(a) - sizeOptions.indexOf(b)).forEach(({size ,qty})=> {
         if (!size) throw new Error('상품 재고 사이즈칸을 확인 해주세요.')
         if (qty < 0) throw new Error('수량은 음수가 될 수 없습니다.')
         stock[size] = qty
     })
-    const size = Object.keys(stock).sort((a, b) => sizeOptions.indexOf(a) - sizeOptions.indexOf(b));
+    const size = Object.keys(stock);
     return { stock , size } 
 }
 
